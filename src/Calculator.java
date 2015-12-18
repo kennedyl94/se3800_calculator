faaf0df3ad04f7d769fd9513479fa22d58b03fbb
kennedyl <kennedyl@msoe.edu>
2015-12-18 10:46:26 -0600

    made a main method and a loop to get commands

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class Calculator {
+	public static void main(String args[]) {
+		
+		Scanner s = new Scanner(System.in);
+
+		String command = "";
+		while (!command.equals("EXIT")) {
+			System.out.println("Enter a command: ");
+			command = s.next().toUpperCase();
+			System.out.println(command);
+		}
+		s.close();
+	}
 
 }
